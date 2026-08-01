fa5b8476bbfea1e7cdaa8a96d4db51c03e848283
hansschaa <hansschaadev@gmail.com>
2026-08-01 15:09:31 -0400

    fix: set matplotlib backend to Agg to prevent X11 display hangs on headless lab servers

--- scratch/run_ablation_es_surrogate.py
+++ scratch/run_ablation_es_surrogate.py
@@ -5,6 +5,8 @@ import sys
 import pandas as pd
 import numpy as np
 import json
+import matplotlib
+matplotlib.use('Agg') # Evita cuelgues en servidores headless sin servidor X11/GUI
 import matplotlib.pyplot as plt
 import seaborn as sns
 
