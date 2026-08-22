70491a0c78372ba7908e9789fd225706f8afd8f7
hansschaa <hansschaadev@gmail.com>
2026-08-22 12:13:14 -0400

    Completely remove global timeout for python subprocess

--- run_benchmark.py
+++ run_benchmark.py
@@ -78,7 +78,7 @@ def main():
             
             try:
                 # Damos 10 minutos para que complete todos los tableros de una heurística
-                subprocess.run(cmd, env=env, check=True, timeout=7200)
+                subprocess.run(cmd, env=env, check=True)
             except subprocess.TimeoutExpired:
                 print(f"TIMEOUT EXPIRED for {heuristic}!")
             except subprocess.CalledProcessError as e:
